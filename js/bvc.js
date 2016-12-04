b0e28d26b0ac924635d542d5cb3a28aaaf867d20
apolan <afpolani@gmail.com>
2016-12-04 01:18:15 -0500

    brush and zoom

--- js/bvc.js
+++ js/bvc.js
@@ -22,6 +22,8 @@ width = windowWidth - margin.left - margin.right,
 
 var modelTag = "cantidad";
 var segmentTag = "variable";
+var accionTag = "";
+
 var highlightColor = "red";
 
 var treemap = d3.layout.treemap()
@@ -79,10 +81,11 @@ var y = d3.scale.linear()
 
 var clickCount = 0;
 var mouseClick = function (d) {
-
+    hideTooltip();
     var xPosition = d3.event.pageX;
     var yPosition = d3.event.pageY;
 
+    accionTag = d.NEMO;
     // stop tooltip going off the right or bottom side of screen
     if (xPosition > (width / 2)) {
         xPosition = xPosition - 242;
@@ -96,12 +99,6 @@ var mouseClick = function (d) {
     clickCount++;
     d3.selectAll(".node-website").style("box-shadow", "none");
     d3.select(this).style("box-shadow", "inset 0 0 10px white");
-   /* d3.select(this).transition()
-            .duration(750)
-            .attr("transform", "translate(480,480)scale(23)rotate(180)").transition()
-      .delay(1500)
-      .attr("transform", "translate(240,240)scale(0)")
-     );/*/
 
     d3.select("#tooltip")
             .style("left", xPosition + "px")
@@ -127,6 +124,7 @@ var mouseClick = function (d) {
         hideTooltip();
         d3.selectAll(".node-website").style("box-shadow", "none");
     }
+    loadBrush();
 };
 
 // Se carga csv
@@ -159,16 +157,16 @@ d3.csv("acciones.csv", function (data) {
                 if (d.y - cellpadding === d.parent.y && d.x - cellpadding === d.parent.x) {
                     return d.parent.key;
                 }
-            })
-            .style("z-index", function (d) {
-                if (d.y - cellpadding === d.parent.y && d.x - cellpadding === d.parent.x) {
-                    return 99;
-                }
-            });
+            }).style("z-index", function (d) {
+        if (d.y - cellpadding === d.parent.y && d.x - cellpadding === d.parent.x) {
+            return 99;
+        }
+    }).style("font-size", "12pt")
+            .style("margin-top", "5px");
 
     customeAnimation = function (option) {
 
-        hideTooltip();
+        //hideTooltip();
         var value = function () {
             return 1;
         }
@@ -185,6 +183,10 @@ d3.csv("acciones.csv", function (data) {
             value = function (d) {
                 return d.MONTO;
             };
+        } else if (option === 'alfabeticamente') {
+            value = function (d) {
+                return 50;
+            };
         } else {
             value = function (d) {
                 return d.CANTIDAD;
@@ -196,150 +198,6 @@ d3.csv("acciones.csv", function (data) {
                 .duration(3500)
                 .call(position);
     };
-    /*
-     initialize(treeData);
-     accumulate(treeData);
-     layout(treeData);*/
-    display(treeData);
-
-    function initialize(root) {
-        root.dx = width;
-        root.dy = height;
-        root.depth = 0;
-    }
-
-    // Aggregate the values for internal nodes. This is normally done by the
-    // treemap layout, but not here because of our custom implementation.
-    function accumulate(d) {
-        //console.log(d.children);
-        return d.values
-                ? d.value = d.values.reduce(function (p, v) {
-                    return p + accumulate(v);
-                }, 0)
-                : +d.value;
-    }
-
-    // Compute the treemap layout recursively such that each group of siblings
-    // uses the same size (1×1) rather than the dimensions of the parent cell.
-    // This optimizes the layout for the current zoom state. Note that a wrapper
-    // object is created for the parent node for each group of siblings so that
-    // the parent’s dimensions are not discarded as we recurse. Since each group
-    // of sibling was laid out in 1×1, we must rescale to fit using absolute
-    // coordinates. This lets us use a viewport to zoom.
-    function layout(d) {
-        if (d.values) {
-            treemap.nodes({values: d.values});
-            d.values.forEach(function (c) {
-                c.x = d.x + c.x * d.dx;
-                c.y = d.y + c.y * d.dy;
-                c.dx *= d.dx;
-                c.dy *= d.dy;
-                c.parent = d;
-                layout(c);
-            });
-        }
-    }
-
-    function display(d) {
-        grandparent.datum(d.parent)
-                .on("click", transition);
-        //.select("text")
-        //.text(name(d));
-
-        var g = d3.selectAll(".node-website");
-        // .data(d.values);
-
-        g.filter(function (d) {
-            return d.values;
-        }).classed("children", true).on("click", transition);
-
-        /*g.selectAll(".child")
-         .data(function (d) {
-         return d.values || [d];
-         })
-         .enter().append("rect")
-         .attr("class", "child")
-         .call(rect);
-         /*/
-        /*     g.append("rect")
-         .attr("class", "parent")
-         .call(rect)
-         .append("title")
-         .text(function (d) {
-         return formatNumber(+d.value);
-         });
-         
-         g.append("text")
-         .attr("dy", ".75em")
-         .text(function (d) {
-         return name(d);
-         })
-         .call(text);
-         */
-        function transition(d) {
-            if (transitioning || !d)
-                return;
-            transitioning = true;
-
-            var g2 = display(d),
-                    t1 = g1.transition().duration(750),
-                    t2 = g2.transition().duration(750);
-
-            // Update the domain only after entering new elements.
-            x.domain([d.x, d.x + d.dx]);
-            y.domain([d.y, d.y + d.dy]);
-
-            // Enable anti-aliasing during the transition.
-            div.style("shape-rendering", null);
-
-            /*// Draw child nodes on top of parent nodes.
-             div.selectAll(".depth").sort(function (a, b) {
-             return a.depth - b.depth;
-             });*/
-
-            // Fade-in entering text.
-            g2.selectAll("text").style("fill-opacity", 0);
-
-            // Transition to the new view.
-            t1.selectAll("text").call(text).style("fill-opacity", 0);
-            t2.selectAll("text").call(text).style("fill-opacity", 1);
-            t1.selectAll("rect").call(rect);
-            t2.selectAll("rect").call(rect);
-
-            // Remove the old node when the transition is finished.
-            t1.remove().each("end", function () {
-                div.style("shape-rendering", "crispEdges");
-                transitioning = false;
-            });
-        }
-
-        return g;
-    }
-
-    function text(text) {
-        text.attr("x", function (d) {
-            return x(d.x) + 6;
-        })
-                .attr("y", function (d) {
-                    return y(d.y) + 6;
-                });
-    }
-
-    function rect(rect) {
-        rect.attr("x", function (d) {
-            return x(d.x);
-        })
-                .attr("y", function (d) {
-                    return y(d.y);
-                })
-                .attr("width", function (d) {
-                    return x(d.x + d.dx) - x(d.x);
-                })
-                .attr("height", function (d) {
-                    return y(d.y + d.dy) - y(d.y);
-                });
-    }
-
 
 
     customeAnimation();
@@ -352,6 +210,7 @@ d3.csv("acciones.csv", function (data) {
 
 function hideTooltip() {
     d3.select("#tooltip").classed("hidden", true);
+    d3.selectAll("svg").remove();
 }
 
 function position() {
@@ -440,6 +299,154 @@ function updateColor() {
                         return  "#9BA9B2";
                     }
                     return "#B4BEC5";
+                } else if (modelTag === "alfabeticamente") {
+
+                    return "#697E8C";
                 }
             });
+}
+
+
+
+
+function loadBrush() {
+
+    var margin = {top: 10, right: 10, bottom: 100, left: 80},
+    margin2 = {top: 430, right: 10, bottom: 20, left: 80},
+    width = 600 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom,
+            height2 = 500 - margin2.top - margin2.bottom;
+
+    //var parseDate = d3.time.format("%Y-%d-%m").parse;
+    var parseDate = d3.time.format("%b %d %Y").parse;
+
+    var x = d3.time.scale().range([0, width]),
+            x2 = d3.time.scale().range([0, width]),
+            y = d3.scale.linear().range([height, 0]),
+            y2 = d3.scale.linear().range([height2, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+            xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+            yAxis = d3.svg.axis().scale(y).orient("left");
+
+    var brush = d3.svg.brush()
+            .x(x2)
+            .on("brush", brushed);
+
+    var area = d3.svg.area()
+            .interpolate("monotone")
+            .x(function (d) {
+                return x(d.fecha);
+            }).y0(height)
+            .y1(function (d) {
+                return y(d.Cantidad);
+            });
+
+    var area2 = d3.svg.area()
+            .interpolate("monotone")
+            .x(function (d) {
+                return x2(d.fecha);
+            }).y0(height2)
+            .y1(function (d) {
+                return y2(d.Cantidad);
+            });
+
+    var svg = d3.select("#chart").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom);
+
+    svg.append("defs").append("clipPath")
+            .attr("id", "clip")
+            .append("rect")
+            .attr("width", width)
+            .attr("height", height);
+
+    var focus = svg.append("g")
+            .attr("class", "focus")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var context = svg.append("g")
+            .attr("class", "context")
+            .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+    var zoom = d3.behavior.zoom()
+            .on("zoom", draw);
+// Add rect cover the zoomed graph and attach zoom event.
+    var rect = svg.append("svg:rect")
+            .attr("class", "pane")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+            .call(zoom);
+
+    d3.csv("data/hist/" + accionTag + "_HIST.csv", type, function (error, data) {
+        x.domain(d3.extent(data.map(function (d) {
+            return d.fecha;
+        })));
+        y.domain([0, d3.max(data.map(function (d) {
+                return d.Cantidad;
+            }))]);
+        x2.domain(x.domain());
+        y2.domain(y.domain());
+
+        // Set up zoom behavior
+        zoom.x(x);
+
+        focus.append("path")
+                .datum(data)
+                .attr("class", "area")
+                .attr("id", function (d) {
+                    return d.Nemotecnico;
+                })
+                .attr("d", area);
+
+        focus.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height + ")")
+                .call(xAxis);
+
+        focus.append("g")
+                .attr("class", "y axis")
+                .call(yAxis);
+
+        context.append("path")
+                .datum(data)
+                .attr("class", "area")
+                .attr("d", area2);
+
+        context.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0," + height2 + ")")
+                .call(xAxis2);
+
+        context.append("g")
+                .attr("class", "x brush")
+                .call(brush)
+                .selectAll("rect")
+                .attr("y", -6)
+                .attr("height", height2 + 7);
+    });
+
+    function brushed() {
+        x.domain(brush.empty() ? x2.domain() : brush.extent());
+        focus.select(".area").attr("d", area);
+        focus.select(".x.axis").call(xAxis);
+        // Reset zoom scale's domain
+        zoom.x(x);
+    }
+
+    function draw() {
+        focus.select(".area").attr("d", area);
+        focus.select(".x.axis").call(xAxis);
+        // Force changing brush range
+        brush.extent(x.domain());
+        svg.select(".brush").call(brush);
+    }
+
+    function type(d) {
+        //d.fecha = parseDate(d.fecha);
+        d.fecha = parseDate(d.fecha);
+        d.Cantidad = +d.Cantidad;
+        return d;
+    }
 }
